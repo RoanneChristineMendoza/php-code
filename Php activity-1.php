b19952b88cb74dc3b8fb8b1b8591b21435c32f6a
RoanneChristineMendoza <roanneamendoza@gmail.com>
2024-09-06 21:59:25 +0800

    Update Php activity-1.php

--- Php activity-1.php
+++ Php activity-1.php
@@ -1,7 +1,7 @@
 <?php
 
 echo file_get_contents("test.txt");
-echo file_put_contents("test.txt","Hello World. Testing!");
+echo file_put_contents("test.txt","Grp3_Roannemendoza_ex3");
 echo file_exists("webdictionary.txt");
 print_r(file("test.txt"));
 
